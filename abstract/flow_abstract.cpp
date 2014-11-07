a8404aa75dfc97e80ea3a7817255bae5c72ef0b8
Yihua Guo <yihua.guo.gcg@gmail.com>
2014-11-06 22:24:45 -0500

    remove idle flows

--- abstract/flow_abstract.cpp
+++ abstract/flow_abstract.cpp
@@ -169,9 +169,15 @@ void FlowAbstract::runMeasureTask(Result* result, Context& traceCtx, const struc
 
 		if (userp->is_sample && ts - userp->cc_start > 1.0) {
 			int concurrency = 0;
-			for (flow_it = userp->tcp_flows.begin(); flow_it != userp->tcp_flows.end(); flow_it++) {
-				if (flow_it->second->last_data_time > userp->cc_start)
+			for (flow_it = userp->tcp_flows.begin(); flow_it != userp->tcp_flows.end();) {
+				if (flow_it->second->last_data_time > userp->cc_start) {
+					flow_it++;
 					concurrency++;
+				} else if (ts - flow_it->second->last_data_time > FLOW_MAX_IDLE_TIME) {
+					userp->tcp_flows.erase(flow_it++);
+				} else {
+					flow_it++;
+				}
 			}
 			if (concurrency > 0) {
 				char buf[100];
